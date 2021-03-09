58b5223b7e716a978d84aac735c5045025157d16
Piwero <piwero@gmail.com>
2021-03-09 16:10:54 +0000

    create empyt list, messages and push them to the list

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Welcome to my bookshop");
+const messages = [];
+
+let message1 = "Welcome to my book shop!";
+let message2 = "Such a lovely day!";
+let message3 = "Have you read the last Obama's book?";
+let message4 = "We got some old books for ypung people like you!" ;
+let message5= "This is a bookshop, not a library!";
+
+messages.push(message1, message2, message3, message4, message5);
+
+
