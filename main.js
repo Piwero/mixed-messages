f1b28c6fd34dcdafe9278a8bba418970b8a17c98
Piwero <piwero@gmail.com>
2021-03-15 09:31:58 +0000

    test Node messages

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const messages = [];
-
-let message1 = "Welcome to my book shop!";
-let message2 = "Such a lovely day!";
-let message3 = "Have you read the last Obama's book?";
-let message4 = "We got some old books for ypung people like you!" ;
-let message5= "This is a bookshop, not a library!";
-
-messages.push(message1, message2, message3, message4, message5);
 
 
+const mixedMessages = () =>{
+    let messages = [];
+
+    
+
+    let message1 = "Welcome to my bookshop!";
+    let message2 = "Such a lovely day!";
+    let message3 = "Have you read the last Obama's book?";
+    let message4 = "We got some old books for young people like you!" ;
+    let message5= "This is a bookshop, not a library!";
+
+    messages.push(message1, message2, message3, message4, message5);
+    let random = Math.floor(Math.random() * messages.length)
+
+    console.log(messages[random]);
+
+}
+
+
+mixedMessages();
